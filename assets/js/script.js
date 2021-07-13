a103c9be4199b98fd390ace21202f972b65a0c1c
Alex Harrington <spirithoula2@gmail.com>
2021-07-13 17:15:08 -0500

    edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var startButton = document.getElementById("start");
-var timer = document.querySelector(".timer-count");
+var timer = document.querySelector("timer");
 var quizContainer = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
 var submitButton = document.getElementById("submit");
@@ -70,10 +70,53 @@ timer.addEventListener("click", function () {
 
       if (timeLeft <= 0) {
         clearTimeout(defaultTime);
-        currentTime.textContent = "Time Over!";
+        currentTime.textContent = "Time's Up!";
       }
     }, 1000);
   }
   render(questionsIndex);
 });
 
+function render(questionsIndex) {
+  questionsDiv.innerHTML = "";
+  createUl.innerHTML = "";
+
+  for (i = 0; i < questions.length; i++) {
+    var userQuestion = questions[questionsIndex].title;
+    var userChoices = questions[questionsIndex].choices;
+    questionsDiv.textContent = userQuestion;
+  }
+  userChoices.forEach(function (newItem) {
+    var listItem = document.createElement("li");
+    listItem.textContent = newItem;
+    questionsDiv.appendChild(createUl);
+    createUl.appendChild(listItem);
+    listItem.addEventListener("click", compare);
+  });
+}
+
+function compare(event) {
+  var element = event.target;
+
+  if (element.matches("li")) {
+    var createDiv = document.createElement("div");
+    createDiv.setAttribute("id", "createDiv");
+    //this calls back the answer in the array to distinguish right answers out of the text content
+    if (element.textContent === questions[questionsIndex].answer) {
+      score++;
+      createDiv.textContent = "Correct Answer!";
+    } else {
+      timeLeft = timeLeft - penalty;
+      createDiv.textContent = "Incorrect, the answer is " + questions[questionsIndex].answer;
+    }
+  }
+  questionsIndex++;
+
+  if (questionsIndex >= questions.length) {
+    quizOver();
+    createDiv.textContent = "You got right:" + score + "/" + questions.length;
+  } else {
+    render(questionsIndex);
+  }
+  questionsDiv.appendChild(createDiv);
+}
